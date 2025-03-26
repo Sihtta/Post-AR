88b56cb3a1030d8764e39d08dd538b249bffcd1a
JeanBon-dev <hugues.porchet@orange.fr>
2025-03-26 09:33:51 +0100

    MobileNavbar for all pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import MobileNavbar from '../components/MobileNavbar';
 import { Link, useLocation } from 'react-router-dom';
 import styles from '../styles/navbar.module.css';
 import { FaRegUser, FaRegCreditCard } from 'react-icons/fa';
 
-const Navbar: React.FC = () => {
+const Navbar = () => {
   const location = useLocation();
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
-  return (
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 768);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return isMobile ? <MobileNavbar /> : (
     <nav className={styles.navbar}>
       <h1 className={styles.logo}>Post AR</h1>
       <div className={styles.links}>
-      <Link to="/dashboard">Dashboard</Link>
+        <Link to="/dashboard">Dashboard</Link>
         <Link to="/cards" className={`${styles.link} ${location.pathname === '/cards' ? styles.active : ''}`}>
           <FaRegCreditCard /> Cards
         </Link>
